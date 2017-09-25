dedc6993fd75e61a5933aad3776f21c383105e2a
Benjamin Otte <otte@redhat.com>
2017-09-25 15:09:39 +0200

    gsksl: Introduce GskSlValue
    This is - analog to a GValue - a type plus the memory of a value in that
    type.
    
    So far it is only used to represent constant expressions.

--- gsk/gsksltypeprivate.h
+++ gsk/gsksltypeprivate.h
@@ -53,6 +53,7 @@ const char *            gsk_sl_type_get_name                    (const GskSlType
 GskSlScalarType         gsk_sl_type_get_scalar_type             (const GskSlType     *type);
 GskSlType *             gsk_sl_type_get_index_type              (const GskSlType     *type);
 guint                   gsk_sl_type_get_length                  (const GskSlType     *type);
+gsize                   gsk_sl_type_get_size                    (const GskSlType     *type);
 gboolean                gsk_sl_scalar_type_can_convert          (GskSlScalarType      target,
                                                                  GskSlScalarType      source);
 gboolean                gsk_sl_type_can_convert                 (const GskSlType     *target,
@@ -64,6 +65,13 @@ guint                   gsk_sl_type_hash                        (gconstpointer  
 
 guint32                 gsk_sl_type_write_spv                   (const GskSlType     *type,
                                                                  GskSpvWriter        *writer);
+
+void                    gsk_sl_type_print_value                 (const GskSlType     *type,
+                                                                 GString             *string,
+                                                                 gpointer             value);
+guint32                 gsk_sl_type_write_value_spv             (GskSlType           *type,
+                                                                 GskSpvWriter        *writer,
+                                                                 gpointer             value);
 
 G_END_DECLS
 
