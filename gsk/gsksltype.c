yatima1460/gtk

gsksl: Add gsksltypesprivate.h
All those types are recursively needing each other in their functions,
so add a generic header for all those types and only include that
header.

gsk/gsksltype.c
@@ -20,6 +20,9 @@
 
 #include "gsksltypeprivate.h"
 
+#include "gsksltokenizerprivate.h"
+#include "gsksltokenstreamprivate.h"
+
 #include <string.h>
 
 struct _GskSlType

gsk/gsksltypesprivate.h
@@ -1,0 +1,28 @@
+/* GSK - The GTK Scene Kit
+ * Copyright 2016  Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GSK_SL_TYPES_H__
+#define __GSK_SL_TYPES_H__
+
+#include <gsk/gsktypes.h>
+
+typedef struct _GskSlNode               GskSlNode;
+typedef struct _GskSlToken              GskSlToken;
+typedef struct _GskSlTokenStream        GskSlTokenStream;
+typedef struct _GskSlType               GskSlType;
+
+#endif /* __GSK_SL_TYPES_H__ */

federicomenaquintero/gtk-1

gsk/gsksltype.c
@@ -20,6 +20,9 @@
 
 #include "gsksltypeprivate.h"
 
+#include "gsksltokenizerprivate.h"
+#include "gskslpreprocessorprivate.h"
+
 #include <string.h>
 
 struct _GskSlType

gsk/gsksltypesprivate.h
@@ -1,0 +1,28 @@
+/* GSK - The GTK Scene Kit
+ * Copyright 2016  Benjamin Otte
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GSK_SL_TYPES_H__
+#define __GSK_SL_TYPES_H__
+
+#include <gsk/gsktypes.h>
+
+typedef struct _GskSlNode               GskSlNode;
+typedef struct _GskSlPreprocessor       GskSlPreprocessor;
+typedef struct _GskSlToken              GskSlToken;
+typedef struct _GskSlType               GskSlType;
+
+#endif /* __GSK_SL_TYPES_H__ */