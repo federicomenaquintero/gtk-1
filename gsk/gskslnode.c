yatima1460/gtk

gsk: Add a skeleton for a GLSL parser

gsk/gskslnode.c
@@ -1,0 +1,285 @@
+/* GTK - The GIMP Toolkit
+ *   
+ * Copyright © 2017 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "gskslnodeprivate.h"
+
+#include "gsksltokenstreamprivate.h"
+#include "gsksltypeprivate.h"
+
+#include <string.h>
+
+static GskSlNode *
+gsk_sl_node_alloc (const GskSlNodeClass *klass,
+                   gsize                 size)
+{
+  GskSlNode *node;
+
+  node = g_slice_alloc0 (size);
+
+  node->class = klass;
+  node->ref_count = 1;
+
+  return node;
+}
+#define gsk_sl_node_new(_name, _klass) ((_name *) gsk_sl_node_alloc ((_klass), sizeof (_name)))
+
+/* PROGRAM */
+
+typedef struct _GskSlNodeProgram GskSlNodeProgram;
+
+struct _GskSlNodeProgram {
+  GskSlNode parent;
+
+  GSList *declarations;
+  GSList *functions;
+};
+
+static void
+gsk_sl_node_program_free (GskSlNode *node)
+{
+  GskSlNodeProgram *program = (GskSlNodeProgram *) node;
+
+  g_slist_free (program->declarations);
+  g_slist_free (program->functions);
+
+  g_slice_free (GskSlNodeProgram, program);
+}
+
+static void
+gsk_sl_node_program_print (GskSlNode *node,
+                           GString   *string)
+{
+  GskSlNodeProgram *program = (GskSlNodeProgram *) node;
+  GSList *l;
+
+  for (l = program->declarations; l; l = l->next)
+    gsk_sl_node_print (l->data, string);
+
+  for (l = program->functions; l; l = l->next)
+    gsk_sl_node_print (l->data, string);
+}
+
+static const GskSlNodeClass GSK_SL_NODE_PROGRAM = {
+  gsk_sl_node_program_free,
+  gsk_sl_node_program_print
+};
+
+/* FUNCTION */
+
+typedef struct _GskSlNodeFunction GskSlNodeFunction;
+
+struct _GskSlNodeFunction {
+  GskSlNode parent;
+
+  GskSlType *return_type;
+  char *name;
+};
+
+static void
+gsk_sl_node_function_free (GskSlNode *node)
+{
+  GskSlNodeFunction *function = (GskSlNodeFunction *) node;
+
+  if (function->return_type)
+    gsk_sl_type_unref (function->return_type);
+  g_free (function->name);
+
+  g_slice_free (GskSlNodeFunction, function);
+}
+
+static void
+gsk_sl_node_function_print (GskSlNode *node,
+                            GString   *string)
+{
+  GskSlNodeFunction *function = (GskSlNodeFunction *) node;
+
+  gsk_sl_type_print (function->return_type, string);
+  g_string_append (string, "\n");
+
+  g_string_append (string, function->name);
+  g_string_append (string, " (");
+  g_string_append (string, ")\n");
+
+  g_string_append (string, "{\n");
+  g_string_append (string, "}\n");
+}
+
+static const GskSlNodeClass GSK_SL_NODE_FUNCTION = {
+  gsk_sl_node_function_free,
+  gsk_sl_node_function_print
+};
+
+/* API */
+
+static GskSlNodeFunction *
+gsk_sl_node_parse_function_prototype (GskSlNodeProgram *program,
+                                      GskSlTokenStream *stream)
+{
+  GskSlType *type;
+  GskSlNodeFunction *function;
+  const GskSlToken *token;
+
+  type = gsk_sl_type_new_parse (stream);
+  if (type == NULL)
+    return NULL;
+
+  token = gsk_sl_token_stream_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_IDENTIFIER))
+    {
+      gsk_sl_token_stream_error (stream, "Expected a function name");
+      gsk_sl_type_unref (type);
+      return NULL;
+    }
+
+  function = gsk_sl_node_new (GskSlNodeFunction, &GSK_SL_NODE_FUNCTION);
+  function->return_type = type;
+  function->name = g_strdup (token->str);
+  gsk_sl_token_stream_consume (stream, (GskSlNode *) function);
+
+  token = gsk_sl_token_stream_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_LEFT_PAREN))
+    {
+      gsk_sl_token_stream_error (stream, "Expected an openening \"(\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_token_stream_consume (stream, (GskSlNode *) function);
+
+  token = gsk_sl_token_stream_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_RIGHT_PAREN))
+    {
+      gsk_sl_token_stream_error (stream, "Expected a closing \")\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_token_stream_consume (stream, (GskSlNode *) function);
+
+  return function;
+}
+
+static gboolean
+gsk_sl_node_parse_function_definition (GskSlNodeProgram *program,
+                                       GskSlTokenStream *stream)
+{
+  GskSlNodeFunction *function;
+  const GskSlToken *token;
+
+  function = gsk_sl_node_parse_function_prototype (program, stream);
+  if (function == NULL)
+    return FALSE;
+
+  token = gsk_sl_token_stream_get (stream);
+  if (gsk_sl_token_is (token, GSK_SL_TOKEN_SEMICOLON))
+    {
+      gsk_sl_token_stream_consume (stream, (GskSlNode *) function);
+      program->functions = g_slist_prepend (program->functions, function);
+      return TRUE;
+    }
+
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_LEFT_BRACE))
+    {
+      gsk_sl_token_stream_error (stream, "Expected an opening \"{\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_token_stream_consume (stream, (GskSlNode *) function);
+
+  token = gsk_sl_token_stream_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_RIGHT_BRACE))
+    {
+      gsk_sl_token_stream_error (stream, "Expected a closing \"}\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_token_stream_consume (stream, (GskSlNode *) function);
+
+  program->functions = g_slist_prepend (program->functions, function);
+
+  return TRUE;
+}
+
+static gboolean
+gsk_sl_node_parse_program (GskSlNodeProgram *program,
+                           GskSlTokenStream *stream)
+{
+  const GskSlToken *token;
+  gboolean result = TRUE;
+
+  for (token = gsk_sl_token_stream_get (stream);
+       !gsk_sl_token_is (token, GSK_SL_TOKEN_EOF);
+       token = gsk_sl_token_stream_get (stream))
+    {
+      if (!gsk_sl_node_parse_function_definition (program, stream))
+        {
+          gsk_sl_token_stream_consume (stream, (GskSlNode *) program);
+          result = FALSE;
+        }
+    }
+
+  return result;
+}
+
+
+GskSlNode *
+gsk_sl_node_new_program (GBytes  *source,
+                         GError **error)
+{
+  GskSlTokenStream *stream;
+  GskSlNodeProgram *program;
+
+  program = gsk_sl_node_new (GskSlNodeProgram, &GSK_SL_NODE_PROGRAM);
+  stream = gsk_sl_token_stream_new (source);
+
+  gsk_sl_node_parse_program (program, stream);
+
+  gsk_sl_token_stream_unref (stream);
+
+  return (GskSlNode *) program;
+}
+
+GskSlNode *
+gsk_sl_node_ref (GskSlNode *node)
+{
+  g_return_val_if_fail (node != NULL, NULL);
+
+  node->ref_count += 1;
+
+  return node;
+}
+
+void
+gsk_sl_node_unref (GskSlNode *node)
+{
+  if (node == NULL)
+    return;
+
+  node->ref_count -= 1;
+  if (node->ref_count > 0)
+    return;
+
+  node->class->free (node);
+}
+
+void
+gsk_sl_node_print (GskSlNode *node,
+                   GString   *string)
+{
+  node->class->print (node, string);
+}

gsk/gsksltype.c
@@ -1,0 +1,161 @@
+/* GTK - The GIMP Toolkit
+ *   
+ * Copyright © 2017 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "gsksltypeprivate.h"
+
+#include <string.h>
+
+struct _GskSlType
+{
+  int ref_count;
+
+  GskSlBuiltinType builtin;
+};
+
+static GskSlType
+builtin_types[GSK_SL_N_BUILTIN_TYPES] = {
+  [GSK_SL_VOID] = { 1, GSK_SL_VOID },
+  [GSK_SL_FLOAT] = { 1, GSK_SL_FLOAT },
+  [GSK_SL_DOUBLE] = { 1, GSK_SL_DOUBLE },
+  [GSK_SL_INT] = { 1, GSK_SL_INT },
+  [GSK_SL_UINT] = { 1, GSK_SL_UINT },
+  [GSK_SL_BOOL] = { 1, GSK_SL_BOOL },
+  [GSK_SL_VEC2] = { 1, GSK_SL_VEC2 },
+  [GSK_SL_VEC3] = { 1, GSK_SL_VEC3 },
+  [GSK_SL_VEC4] = { 1, GSK_SL_VEC4 }
+};
+
+GskSlType *
+gsk_sl_type_new_parse (GskSlTokenStream *stream)
+{
+  GskSlBuiltinType builtin;
+  const GskSlToken *token;
+
+  token = gsk_sl_token_stream_get (stream);
+
+  switch (token->type)
+  {
+    case GSK_SL_TOKEN_VOID:
+      builtin = GSK_SL_VOID;
+      break;
+    case GSK_SL_TOKEN_FLOAT:
+      builtin = GSK_SL_FLOAT;
+      break;
+    case GSK_SL_TOKEN_DOUBLE:
+      builtin = GSK_SL_DOUBLE;
+      break;
+    case GSK_SL_TOKEN_INT:
+      builtin = GSK_SL_INT;
+      break;
+    case GSK_SL_TOKEN_UINT:
+      builtin = GSK_SL_UINT;
+      break;
+    case GSK_SL_TOKEN_BOOL:
+      builtin = GSK_SL_BOOL;
+      break;
+    case GSK_SL_TOKEN_VEC2:
+      builtin = GSK_SL_VEC2;
+      break;
+    case GSK_SL_TOKEN_VEC3:
+      builtin = GSK_SL_VEC3;
+      break;
+    case GSK_SL_TOKEN_VEC4:
+      builtin = GSK_SL_VEC4;
+      break;
+    default:
+      gsk_sl_token_stream_error (stream, "Expected type specifier");
+      return NULL;
+  }
+
+  gsk_sl_token_stream_consume (stream, NULL);
+  return gsk_sl_type_ref (gsk_sl_type_get_builtin (builtin));
+}
+
+GskSlType *
+gsk_sl_type_get_builtin (GskSlBuiltinType builtin)
+{
+  g_assert (builtin < GSK_SL_N_BUILTIN_TYPES);
+
+  return &builtin_types[builtin];
+}
+
+GskSlType *
+gsk_sl_type_ref (GskSlType *type)
+{
+  g_return_val_if_fail (type != NULL, NULL);
+
+  type->ref_count += 1;
+
+  return type;
+}
+
+void
+gsk_sl_type_unref (GskSlType *type)
+{
+  if (type == NULL)
+    return;
+
+  type->ref_count -= 1;
+  if (type->ref_count > 0)
+    return;
+
+  g_assert_not_reached ();
+}
+
+void
+gsk_sl_type_print (const GskSlType *type,
+                   GString         *string)
+{
+  switch (type->builtin)
+  {
+    case GSK_SL_VOID:
+      g_string_append (string, "void");
+      break;
+    case GSK_SL_FLOAT:
+      g_string_append (string, "float");
+      break;
+    case GSK_SL_DOUBLE:
+      g_string_append (string, "double");
+      break;
+    case GSK_SL_INT:
+      g_string_append (string, "int");
+      break;
+    case GSK_SL_UINT:
+      g_string_append (string, "uint");
+      break;
+    case GSK_SL_BOOL:
+      g_string_append (string, "bool");
+      break;
+    case GSK_SL_VEC2:
+      g_string_append (string, "vec2");
+      break;
+    case GSK_SL_VEC3:
+      g_string_append (string, "vec3");
+      break;
+    case GSK_SL_VEC4:
+      g_string_append (string, "vec4");
+      break;
+    /* add more above */
+    case GSK_SL_N_BUILTIN_TYPES:
+    default:
+      g_assert_not_reached ();
+      break;
+  }
+}

gsk/gsksltypeprivate.h
@@ -1,0 +1,55 @@
+/* GTK - The GIMP Toolkit
+ *
+ * Copyright © 2017 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GSK_SL_TYPE_PRIVATE_H__
+#define __GSK_SL_TYPE_PRIVATE_H__
+
+#include <glib.h>
+
+#include "gsksltokenstreamprivate.h"
+
+G_BEGIN_DECLS
+
+typedef enum {
+  GSK_SL_VOID,
+  GSK_SL_FLOAT,
+  GSK_SL_DOUBLE,
+  GSK_SL_INT,
+  GSK_SL_UINT,
+  GSK_SL_BOOL,
+  GSK_SL_VEC2,
+  GSK_SL_VEC3,
+  GSK_SL_VEC4,
+  /* add more above */
+  GSK_SL_N_BUILTIN_TYPES
+} GskSlBuiltinType;
+
+typedef struct _GskSlType GskSlType;
+
+GskSlType *             gsk_sl_type_new_parse                   (GskSlTokenStream    *stream);
+GskSlType *             gsk_sl_type_get_builtin                 (GskSlBuiltinType     builtin);
+
+GskSlType *             gsk_sl_type_ref                         (GskSlType           *type);
+void                    gsk_sl_type_unref                       (GskSlType           *type);
+
+void                    gsk_sl_type_print                       (const GskSlType     *type,
+                                                                 GString             *string);
+
+G_END_DECLS
+
+#endif /* __GSK_SL_TYPE_PRIVATE_H__ */

federicomenaquintero/gtk-1

gsk/gskslnode.c
@@ -1,0 +1,285 @@
+/* GTK - The GIMP Toolkit
+ *   
+ * Copyright © 2017 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "gskslnodeprivate.h"
+
+#include "gskslpreprocessorprivate.h"
+#include "gsksltypeprivate.h"
+
+#include <string.h>
+
+static GskSlNode *
+gsk_sl_node_alloc (const GskSlNodeClass *klass,
+                   gsize                 size)
+{
+  GskSlNode *node;
+
+  node = g_slice_alloc0 (size);
+
+  node->class = klass;
+  node->ref_count = 1;
+
+  return node;
+}
+#define gsk_sl_node_new(_name, _klass) ((_name *) gsk_sl_node_alloc ((_klass), sizeof (_name)))
+
+/* PROGRAM */
+
+typedef struct _GskSlNodeProgram GskSlNodeProgram;
+
+struct _GskSlNodeProgram {
+  GskSlNode parent;
+
+  GSList *declarations;
+  GSList *functions;
+};
+
+static void
+gsk_sl_node_program_free (GskSlNode *node)
+{
+  GskSlNodeProgram *program = (GskSlNodeProgram *) node;
+
+  g_slist_free (program->declarations);
+  g_slist_free (program->functions);
+
+  g_slice_free (GskSlNodeProgram, program);
+}
+
+static void
+gsk_sl_node_program_print (GskSlNode *node,
+                           GString   *string)
+{
+  GskSlNodeProgram *program = (GskSlNodeProgram *) node;
+  GSList *l;
+
+  for (l = program->declarations; l; l = l->next)
+    gsk_sl_node_print (l->data, string);
+
+  for (l = program->functions; l; l = l->next)
+    gsk_sl_node_print (l->data, string);
+}
+
+static const GskSlNodeClass GSK_SL_NODE_PROGRAM = {
+  gsk_sl_node_program_free,
+  gsk_sl_node_program_print
+};
+
+/* FUNCTION */
+
+typedef struct _GskSlNodeFunction GskSlNodeFunction;
+
+struct _GskSlNodeFunction {
+  GskSlNode parent;
+
+  GskSlType *return_type;
+  char *name;
+};
+
+static void
+gsk_sl_node_function_free (GskSlNode *node)
+{
+  GskSlNodeFunction *function = (GskSlNodeFunction *) node;
+
+  if (function->return_type)
+    gsk_sl_type_unref (function->return_type);
+  g_free (function->name);
+
+  g_slice_free (GskSlNodeFunction, function);
+}
+
+static void
+gsk_sl_node_function_print (GskSlNode *node,
+                            GString   *string)
+{
+  GskSlNodeFunction *function = (GskSlNodeFunction *) node;
+
+  gsk_sl_type_print (function->return_type, string);
+  g_string_append (string, "\n");
+
+  g_string_append (string, function->name);
+  g_string_append (string, " (");
+  g_string_append (string, ")\n");
+
+  g_string_append (string, "{\n");
+  g_string_append (string, "}\n");
+}
+
+static const GskSlNodeClass GSK_SL_NODE_FUNCTION = {
+  gsk_sl_node_function_free,
+  gsk_sl_node_function_print
+};
+
+/* API */
+
+static GskSlNodeFunction *
+gsk_sl_node_parse_function_prototype (GskSlNodeProgram  *program,
+                                      GskSlPreprocessor *stream)
+{
+  GskSlType *type;
+  GskSlNodeFunction *function;
+  const GskSlToken *token;
+
+  type = gsk_sl_type_new_parse (stream);
+  if (type == NULL)
+    return NULL;
+
+  token = gsk_sl_preprocessor_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_IDENTIFIER))
+    {
+      gsk_sl_preprocessor_error (stream, "Expected a function name");
+      gsk_sl_type_unref (type);
+      return NULL;
+    }
+
+  function = gsk_sl_node_new (GskSlNodeFunction, &GSK_SL_NODE_FUNCTION);
+  function->return_type = type;
+  function->name = g_strdup (token->str);
+  gsk_sl_preprocessor_consume (stream, (GskSlNode *) function);
+
+  token = gsk_sl_preprocessor_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_LEFT_PAREN))
+    {
+      gsk_sl_preprocessor_error (stream, "Expected an openening \"(\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_preprocessor_consume (stream, (GskSlNode *) function);
+
+  token = gsk_sl_preprocessor_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_RIGHT_PAREN))
+    {
+      gsk_sl_preprocessor_error (stream, "Expected a closing \")\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_preprocessor_consume (stream, (GskSlNode *) function);
+
+  return function;
+}
+
+static gboolean
+gsk_sl_node_parse_function_definition (GskSlNodeProgram  *program,
+                                       GskSlPreprocessor *stream)
+{
+  GskSlNodeFunction *function;
+  const GskSlToken *token;
+
+  function = gsk_sl_node_parse_function_prototype (program, stream);
+  if (function == NULL)
+    return FALSE;
+
+  token = gsk_sl_preprocessor_get (stream);
+  if (gsk_sl_token_is (token, GSK_SL_TOKEN_SEMICOLON))
+    {
+      gsk_sl_preprocessor_consume (stream, (GskSlNode *) function);
+      program->functions = g_slist_prepend (program->functions, function);
+      return TRUE;
+    }
+
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_LEFT_BRACE))
+    {
+      gsk_sl_preprocessor_error (stream, "Expected an opening \"{\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_preprocessor_consume (stream, (GskSlNode *) function);
+
+  token = gsk_sl_preprocessor_get (stream);
+  if (!gsk_sl_token_is (token, GSK_SL_TOKEN_RIGHT_BRACE))
+    {
+      gsk_sl_preprocessor_error (stream, "Expected a closing \"}\"");
+      gsk_sl_node_unref ((GskSlNode *) function);
+      return NULL;
+    }
+  gsk_sl_preprocessor_consume (stream, (GskSlNode *) function);
+
+  program->functions = g_slist_prepend (program->functions, function);
+
+  return TRUE;
+}
+
+static gboolean
+gsk_sl_node_parse_program (GskSlNodeProgram  *program,
+                           GskSlPreprocessor *stream)
+{
+  const GskSlToken *token;
+  gboolean result = TRUE;
+
+  for (token = gsk_sl_preprocessor_get (stream);
+       !gsk_sl_token_is (token, GSK_SL_TOKEN_EOF);
+       token = gsk_sl_preprocessor_get (stream))
+    {
+      if (!gsk_sl_node_parse_function_definition (program, stream))
+        {
+          gsk_sl_preprocessor_consume (stream, (GskSlNode *) program);
+          result = FALSE;
+        }
+    }
+
+  return result;
+}
+
+
+GskSlNode *
+gsk_sl_node_new_program (GBytes  *source,
+                         GError **error)
+{
+  GskSlPreprocessor *stream;
+  GskSlNodeProgram *program;
+
+  program = gsk_sl_node_new (GskSlNodeProgram, &GSK_SL_NODE_PROGRAM);
+  stream = gsk_sl_preprocessor_new (source);
+
+  gsk_sl_node_parse_program (program, stream);
+
+  gsk_sl_preprocessor_unref (stream);
+
+  return (GskSlNode *) program;
+}
+
+GskSlNode *
+gsk_sl_node_ref (GskSlNode *node)
+{
+  g_return_val_if_fail (node != NULL, NULL);
+
+  node->ref_count += 1;
+
+  return node;
+}
+
+void
+gsk_sl_node_unref (GskSlNode *node)
+{
+  if (node == NULL)
+    return;
+
+  node->ref_count -= 1;
+  if (node->ref_count > 0)
+    return;
+
+  node->class->free (node);
+}
+
+void
+gsk_sl_node_print (GskSlNode *node,
+                   GString   *string)
+{
+  node->class->print (node, string);
+}

gsk/gsksltype.c
@@ -1,0 +1,161 @@
+/* GTK - The GIMP Toolkit
+ *   
+ * Copyright © 2017 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "gsksltypeprivate.h"
+
+#include <string.h>
+
+struct _GskSlType
+{
+  int ref_count;
+
+  GskSlBuiltinType builtin;
+};
+
+static GskSlType
+builtin_types[GSK_SL_N_BUILTIN_TYPES] = {
+  [GSK_SL_VOID] = { 1, GSK_SL_VOID },
+  [GSK_SL_FLOAT] = { 1, GSK_SL_FLOAT },
+  [GSK_SL_DOUBLE] = { 1, GSK_SL_DOUBLE },
+  [GSK_SL_INT] = { 1, GSK_SL_INT },
+  [GSK_SL_UINT] = { 1, GSK_SL_UINT },
+  [GSK_SL_BOOL] = { 1, GSK_SL_BOOL },
+  [GSK_SL_VEC2] = { 1, GSK_SL_VEC2 },
+  [GSK_SL_VEC3] = { 1, GSK_SL_VEC3 },
+  [GSK_SL_VEC4] = { 1, GSK_SL_VEC4 }
+};
+
+GskSlType *
+gsk_sl_type_new_parse (GskSlPreprocessor *stream)
+{
+  GskSlBuiltinType builtin;
+  const GskSlToken *token;
+
+  token = gsk_sl_preprocessor_get (stream);
+
+  switch (token->type)
+  {
+    case GSK_SL_TOKEN_VOID:
+      builtin = GSK_SL_VOID;
+      break;
+    case GSK_SL_TOKEN_FLOAT:
+      builtin = GSK_SL_FLOAT;
+      break;
+    case GSK_SL_TOKEN_DOUBLE:
+      builtin = GSK_SL_DOUBLE;
+      break;
+    case GSK_SL_TOKEN_INT:
+      builtin = GSK_SL_INT;
+      break;
+    case GSK_SL_TOKEN_UINT:
+      builtin = GSK_SL_UINT;
+      break;
+    case GSK_SL_TOKEN_BOOL:
+      builtin = GSK_SL_BOOL;
+      break;
+    case GSK_SL_TOKEN_VEC2:
+      builtin = GSK_SL_VEC2;
+      break;
+    case GSK_SL_TOKEN_VEC3:
+      builtin = GSK_SL_VEC3;
+      break;
+    case GSK_SL_TOKEN_VEC4:
+      builtin = GSK_SL_VEC4;
+      break;
+    default:
+      gsk_sl_preprocessor_error (stream, "Expected type specifier");
+      return NULL;
+  }
+
+  gsk_sl_preprocessor_consume (stream, NULL);
+  return gsk_sl_type_ref (gsk_sl_type_get_builtin (builtin));
+}
+
+GskSlType *
+gsk_sl_type_get_builtin (GskSlBuiltinType builtin)
+{
+  g_assert (builtin < GSK_SL_N_BUILTIN_TYPES);
+
+  return &builtin_types[builtin];
+}
+
+GskSlType *
+gsk_sl_type_ref (GskSlType *type)
+{
+  g_return_val_if_fail (type != NULL, NULL);
+
+  type->ref_count += 1;
+
+  return type;
+}
+
+void
+gsk_sl_type_unref (GskSlType *type)
+{
+  if (type == NULL)
+    return;
+
+  type->ref_count -= 1;
+  if (type->ref_count > 0)
+    return;
+
+  type->class->free (type);
+}
+
+void
+gsk_sl_type_print (const GskSlType *type,
+                   GString         *string)
+{
+  switch (type->builtin)
+  {
+    case GSK_SL_VOID:
+      g_string_append (string, "void");
+      break;
+    case GSK_SL_FLOAT:
+      g_string_append (string, "float");
+      break;
+    case GSK_SL_DOUBLE:
+      g_string_append (string, "double");
+      break;
+    case GSK_SL_INT:
+      g_string_append (string, "int");
+      break;
+    case GSK_SL_UINT:
+      g_string_append (string, "uint");
+      break;
+    case GSK_SL_BOOL:
+      g_string_append (string, "bool");
+      break;
+    case GSK_SL_VEC2:
+      g_string_append (string, "vec2");
+      break;
+    case GSK_SL_VEC3:
+      g_string_append (string, "vec3");
+      break;
+    case GSK_SL_VEC4:
+      g_string_append (string, "vec4");
+      break;
+    /* add more above */
+    case GSK_SL_N_BUILTIN_TYPES:
+    default:
+      g_assert_not_reached ();
+      break;
+  }
+}

gsk/gsksltypeprivate.h
@@ -1,0 +1,55 @@
+/* GTK - The GIMP Toolkit
+ *
+ * Copyright © 2017 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GSK_SL_TYPE_PRIVATE_H__
+#define __GSK_SL_TYPE_PRIVATE_H__
+
+#include <glib.h>
+
+#include "gskslpreprocessorprivate.h"
+
+G_BEGIN_DECLS
+
+typedef enum {
+  GSK_SL_VOID,
+  GSK_SL_FLOAT,
+  GSK_SL_DOUBLE,
+  GSK_SL_INT,
+  GSK_SL_UINT,
+  GSK_SL_BOOL,
+  GSK_SL_VEC2,
+  GSK_SL_VEC3,
+  GSK_SL_VEC4,
+  /* add more above */
+  GSK_SL_N_BUILTIN_TYPES
+} GskSlBuiltinType;
+
+typedef struct _GskSlType GskSlType;
+
+GskSlType *             gsk_sl_type_new_parse                   (GskSlPreprocessor   *stream);
+GskSlType *             gsk_sl_type_get_builtin                 (GskSlBuiltinType     builtin);
+
+GskSlType *             gsk_sl_type_ref                         (GskSlType           *type);
+void                    gsk_sl_type_unref                       (GskSlType           *type);
+
+void                    gsk_sl_type_print                       (const GskSlType     *type,
+                                                                 GString             *string);
+
+G_END_DECLS
+
+#endif /* __GSK_SL_TYPE_PRIVATE_H__ */