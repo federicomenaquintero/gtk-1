619dddbe4f143730234941f0d6cb9e9d5b7c780a
Federico Mena Quintero <federico@gnome.org>
2017-10-20 15:26:26 -0500

    filechooser: make the test function accept an action and an instance name
    This way we'll be able to test different widgets.

--- testsuite/gtk/filechooser.c
+++ testsuite/gtk/filechooser.c
@@ -3,7 +3,7 @@
 #include <gtk/gtk.h>
 
 static GtkWidget *
-create_save_dialog (void)
+create_file_chooser_dialog (GtkFileChooserAction action)
 {
   GtkWidget *window;
   GtkWidget *dialog;
@@ -12,9 +12,9 @@ create_save_dialog (void)
 
   dialog = gtk_file_chooser_dialog_new ("Hello",
 					GTK_WINDOW (window),
-					GTK_FILE_CHOOSER_ACTION_SAVE,
+					action,
 					"Cancel", GTK_RESPONSE_CANCEL,
-					"Save", GTK_RESPONSE_ACCEPT,
+					"Accept", GTK_RESPONSE_ACCEPT,
 					NULL);
 
   g_signal_connect_swapped (dialog, "destroy",
@@ -24,18 +24,24 @@ create_save_dialog (void)
 }
 
 static void
-test_save_location_entry_is_focused_at_startup (void)
+test_widget_is_focused_at_startup (GtkFileChooserAction action, const char *instance_name)
 {
   GtkWidget *dialog;
   GtkWidget *focused;
 
-  dialog = create_save_dialog ();
+  dialog = create_file_chooser_dialog (action);
   gtk_widget_show_now (dialog);
 
   focused = gtk_window_get_focus (GTK_WINDOW (dialog));
   g_assert (focused != NULL);
 
-  g_assert_cmpstr (g_type_name_from_instance ((GTypeInstance *) focused), ==, "GtkFileChooserEntry");
+  g_assert_cmpstr (g_type_name_from_instance ((GTypeInstance *) focused), ==, instance_name);
+}
+
+static void
+test_save_location_entry_is_focused_at_startup (void)
+{
+  test_widget_is_focused_at_startup (GTK_FILE_CHOOSER_ACTION_SAVE, "GtkFileChooserEntry");
 }
 
 int
